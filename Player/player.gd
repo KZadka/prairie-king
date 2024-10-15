1758a3bb5a97df323b8c2f62e66e0d2b85517557
KZadka <kzadka89@gmail.com>
2024-10-15 15:14:45 +0200

    added: player - move character on board

--- Player/player.gd
+++ Player/player.gd
@@ -1,11 +1,40 @@
 extends Area2D
 
+signal hit
+var speed = 200
+var screen_size
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
+	$Sprite.play()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed('move_up'):
+		velocity.y -= 1
+	if Input.is_action_pressed('move_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('move_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('move_right'):
+		velocity.x += 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	if velocity.x != 0:
+		$Sprite.flip_h = velocity.x < 0
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$Collision.set_deferred('disabled', true)
+	show()
+	
+func start(pos):
+	position = pos
+	$Collision.disabled = false
