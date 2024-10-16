26bd764b6d7bec87b5b975b315e8e9ab74060078
KZadka <kzadka89@gmail.com>
2024-10-16 15:49:22 +0200

    changed: (project, player) - root node from area2d to characterbody2d

--- Player/player.gd
+++ Player/player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends CharacterBody2D
 
 signal hit
 var speed = 200
@@ -6,28 +6,15 @@ var screen_size
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	$Sprite.play()
 
-func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed('move_up'):
-		velocity.y -= 1
-	if Input.is_action_pressed('move_down'):
-		velocity.y += 1
-	if Input.is_action_pressed('move_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('move_right'):
-		velocity.x += 1
-		
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		
-	if velocity.x != 0:
-		$Sprite.flip_h = velocity.x < 0
-		
-	position += velocity * delta
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
+	velocity = direction * speed
+	move_and_slide()
 	position = position.clamp(Vector2.ZERO, screen_size)
-
+	
+func _process(delta: float) -> void:
+	pass
 
 func _on_body_entered(body: Node2D) -> void:
 	hide()
