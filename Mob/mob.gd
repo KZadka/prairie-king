c98fc34c89de42ddcf71023e18e2fb1d703cffd9
KZadka <kzadka89@gmail.com>
2024-10-16 13:34:03 +0200

    added: mob - following player node

--- Mob/mob.gd
+++ Mob/mob.gd
@@ -2,9 +2,11 @@ extends RigidBody2D
 
 var _position_last_frame := Vector2()
 var _cardinal_direction = 0
+var player
 
 func _ready() -> void:
-	pass
+	player = get_tree().get_first_node_in_group('player')
+	$Sprite.play()
 
 func _process(delta: float) -> void:
 	pass
@@ -25,4 +27,5 @@ func _physics_process(delta: float) -> void:
 			$Sprite.rotation_degrees = 90.0
 		3:
 			$Sprite.rotation_degrees = 270.0
+	position += (player.global_position - global_position) / 200
 	_position_last_frame = position
