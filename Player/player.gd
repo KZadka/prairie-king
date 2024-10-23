cf52f568206dac5e7baab492f3708c2d5b31c826
KZadka <kzadka89@gmail.com>
2024-10-23 16:58:34 +0200

    changed: reset

--- Player/player.gd
+++ Player/player.gd
@@ -3,13 +3,24 @@ extends CharacterBody2D
 signal hit
 var speed = 200
 var screen_size
+var attacks = ['side_attack', 'up_attack', 'down_attack']
+
+@onready var state_machine = $AnimationTree['parameters/playback']
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
-	velocity = direction * speed
+	var  current_anim = state_machine.get_current_node()
+	velocity = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down') * speed
+	
+	if velocity.x != 0:
+		$Sprite2D.x = sign(velocity.x)
+	
+	if velocity.length() > 0:
+		state_machine.travel('walk')
+	else:
+		state_machine.travel('idle')
 	move_and_slide()
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
