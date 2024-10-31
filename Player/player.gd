019235c3a413149842e41423180cecf5047ce59c
KZadka <kzadka89@gmail.com>
2024-10-31 14:04:56 +0100

    changed: player - new structure for player scene

--- Player/player.gd
+++ Player/player.gd
@@ -1,38 +1,50 @@
 extends CharacterBody2D
 
-signal hit
-var speed = 200
-var screen_size
-var attacks = ['side_attack', 'up_attack', 'down_attack']
 
-@onready var state_machine = $AnimationTree['parameters/playback']
+const SPEED = 200.0
+var screen_size
+var attacking = false
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-
+	
 func _physics_process(delta: float) -> void:
-	var  current_anim = state_machine.get_current_node()
-	velocity = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down') * speed
-	
+
+	velocity = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down') * SPEED
 	if velocity.x != 0:
-		$Sprite2D.x = sign(velocity.x)
-	
-	if velocity.length() > 0:
-		state_machine.travel('walk')
-	else:
-		state_machine.travel('idle')
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+
+	if not attacking:
+		if velocity.length() > 0:
+			$AnimatedSprite2D.play('walk')
+		else:
+			$AnimatedSprite2D.play('idle')
+	if Input.is_action_just_pressed('attack'):
+		attacking = true
+		if velocity.y < 0:
+			$AnimatedSprite2D.play('up_attack')
+		elif velocity.y > 0:
+			$AnimatedSprite2D.play('down_attack')
+		else:
+			$AnimatedSprite2D.play('side_attack')
+		if not $AnimatedSprite2D.is_playing():
+			$AnimatedSprite2D.animation_finished.emit()
+
 	move_and_slide()
 	position = position.clamp(Vector2.ZERO, screen_size)
-	
-func _process(delta: float) -> void:
-	pass
 
-func _on_body_entered(body: Node2D) -> void:
-	hide()
-	hit.emit()
-	$Collision.set_deferred('disabled', true)
-	show()
-	
 func start(pos):
 	position = pos
-	$Collision.disabled = false
+	$CollisionShape2D.disabled = false
+
+func _on_body_entered(body: Node2D) -> void:
+	pass
+	#print('HIT!!!')
+	#hide()
+	#hit.emit()
+	#$CollisionShape2D.set_deferred('disabled', true)
+	#show()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	attacking = false	
