6d91631066bd021fe8529bc448b86b19ceee724b
KZadka <kzadka89@gmail.com>
2024-10-31 18:16:45 +0100

    changed: mob - type of node and player chase logic

--- Mob/mob.gd
+++ Mob/mob.gd
@@ -1,7 +1,6 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-var _position_last_frame := Vector2()
-var _cardinal_direction = 0
+const SPEED = 100.0
 var player
 
 func _ready() -> void:
@@ -15,17 +14,7 @@ func _on_notifier_screen_exited() -> void:
 	queue_free()
 	
 func _physics_process(delta: float) -> void:
-	$Sprite.rotation_degrees = 0
-	var motion = position - _position_last_frame
-	if motion.length() > 0.0001:
-		_cardinal_direction = int(4.0 * (motion.rotated(PI / 4.0).angle() + PI) / TAU)
-		
-	match _cardinal_direction:
-		0:
-			$Sprite.flip_v = true
-		1:
-			$Sprite.rotation_degrees = 90.0
-		3:
-			$Sprite.rotation_degrees = 270.0
-	position += (player.global_position - global_position) / 200
-	_position_last_frame = position
+	velocity = Vector2.ZERO
+	if player:
+		velocity = position.direction_to(player.position) * SPEED
+	move_and_slide()
